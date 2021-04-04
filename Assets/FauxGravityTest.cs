4376df71801ecd746c996d032f8c503df63e3aa9
Peter Tan <purpl3grape@gmail.com>
2021-04-04 19:25:03 -0400

    Updated Waypoints

--- Assets/FauxGravityTest.cs
+++ Assets/FauxGravityTest.cs
@@ -6,6 +6,13 @@ public class FauxGravityTest : MonoBehaviour
 {
     public Transform attractor;
     public Vector3 _gravityUp;
+    public GameObject[] Waypoints;
+
+    private Transform NextWaypoint;
+    private void Awake()
+    {
+        Waypoints = GameObject.FindGameObjectsWithTag("WayPoint");
+    }
 
     void Update()
     {
@@ -18,7 +25,7 @@ public class FauxGravityTest : MonoBehaviour
         Quaternion _targetRotation = Quaternion.FromToRotation(-transform.up, _gravityUp) * transform.rotation;
         transform.rotation = Quaternion.Slerp(transform.rotation, _targetRotation, 50 * Time.deltaTime);
 
-
+        RotateBicycle();
         //X-Z Inputs Movement
         if (Input.GetKey(KeyCode.W))
         {
@@ -47,5 +54,34 @@ public class FauxGravityTest : MonoBehaviour
             transform.localRotation *= Quaternion.Euler(0, -10, 0);
         }
 
+    }
+
+    float DistanceToPlane;
+    Vector3 DistToWaypoint;
+    Vector3 PointOnPlane;
+    Quaternion q;
+    int waypointIndex = 0;
+    private void RotateBicycle()
+    {
+        NextWaypoint = Waypoints[waypointIndex].transform;
+        DistToWaypoint = NextWaypoint.position - transform.position;
+        if (DistToWaypoint.magnitude < 2)
+        {
+            if (waypointIndex == Waypoints.Length -1)
+            {
+                waypointIndex = 0;
+                Debug.Log("Starting Waypoint");
+            }
+            else
+            {
+                waypointIndex++;
+                Debug.Log("Next Waypoint");
+            }
+        }
+        DistanceToPlane = Vector3.Dot(transform.up, DistToWaypoint);
+        PointOnPlane = NextWaypoint.position - (transform.up * DistanceToPlane);
+        q = Quaternion.LookRotation(PointOnPlane - transform.position, transform.up);
+
+        transform.localRotation = Quaternion.Slerp(transform.rotation, q, 0.2f);
     }
 }
